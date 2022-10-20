a8792da915f4624d47256df50ec0c61c4daf55ce
magiodev <31893902+magiodev@users.noreply.github.com>
2022-10-20 20:39:19 +0200

    Personal changes

--- config/index.js
+++ config/index.js
@@ -2,14 +2,16 @@ export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
   API_URL: "odp-rarity.vercel.app",
-  COLLECTION_NAME: "OneDayPunks",
-  COLLECTION_TITLE: "One Day Punks",
-  COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
+  COLLECTION_NAME: "Cronos Shaggy Galaxy",
+  COLLECTION_TITLE: "Cronos Shaggy Galaxy",
+  COLLECTION_DESCRIPTION:
+    "10.000 unique, cute and shaggy creatures lost in the Shaggy Galaxy! Utility: 🦄 DAO 🪂 Rewards 🕹 GameFi..",
   COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
-  CONTRACT: {
-    // if you're not sure, set CONTRACT: {}, will remove opensea link
-    CHAIN: "ethereum", //possible values of ethereum, matic (polygin), klatyn, solana
-    ADDRESS: "0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91", //note: for solana these values change, so currently not supported
-  },
+  CONTRACT: {},
+  // CONTRACT: {
+  //   // if you're not sure, set CONTRACT: {}, will remove opensea link
+  //   CHAIN: "cronos", //possible values of ethereum, matic (polygin), klatyn, solana
+  //   ADDRESS: "0x6d2da5ae4ef3766c5e327fe3af32c07ef3facd4b", //note: for solana these values change, so currently not supported
+  // },
   WEIGHTS: {},
 };
